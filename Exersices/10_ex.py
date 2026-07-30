11942c30a58ffbadfb204711bd366a2ad577bfff
wedhaka <wedaka@gmail.com>
2026-07-30 23:31:36 +0200

    full function ex 10

--- Exersices/10_ex.py
+++ Exersices/10_ex.py
@@ -23,3 +23,75 @@ def concatenate_rounds(rounds_1, rounds_2):
         list:  All rounds played.
     """
     return rounds_1 + rounds_2
+    
+def list_contains_round(rounds, number):
+    """Check if the list of rounds contains the specified number.
+    Parameters:
+        rounds (list): The rounds played.
+        number (int): The round number.
+    Returns:
+        bool: Was the round played?
+    """
+    if number in rounds :
+        return True
+
+    return False
+
+def card_average(hand):
+    """Calculate and returns the average card value from the list.
+
+    Parameters:
+        hand (list): The cards in the hand.
+
+    Returns:
+        float: The average value of the cards in the hand.
+    """
+    return sum(hand)/len(hand)
+
+def approx_average_is_average(hand):
+    """Return if the (average of first and last card values) OR ('middle' card) == calculated average.
+
+    Parameters:
+        hand (list): The cards in the hand.
+
+    Returns:
+        bool: Does one of the approximate averages equal the `true average`?
+    """
+    card_avg = card_average(hand)
+    new_array = hand[:-1][1:]
+    st_end_array = (hand[1] + hand[-1]) / 2
+
+    print(st_end_array, new_array, card_avg)
+
+    if card_avg == card_average(new_array) :
+        return True
+    elif  st_end_array == card_avg :
+        return True
+    return False
+    
+def average_even_is_average_odd(hand):
+    """Return if the (average of even indexed card values) == (average of odd indexed card values).
+
+    Parameters:
+        hand (list): The cards in the hand.
+
+    Returns:
+        bool: Are the even and odd averages equal?
+    """
+
+    if card_average(hand) == hand[len(hand) // 2]: 
+        return True
+    
+    return False
+
+def maybe_double_last(hand):
+    """Multiply a Jack card value in the last index position by 2.
+
+    Parameters:
+        hand (list): The cards in the hand.
+
+    Returns:
+        list: The hand with Jacks (if present) value doubled.
+    """
+
+    pass
