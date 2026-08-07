9157e18e216e0c78b182e50bf57af7977e4ff302
wedhaka <wedaka@gmail.com>
2026-08-07 23:01:16 +0200

    added 14 ex file and two motheds

---
+++ Exersices/14_ex.py
@@ -0,0 +1,29 @@
+"""Functions to keep track and alter inventory."""
+
+
+def create_inventory(items):
+    """Create a dict that tracks the amount (count) of each element on the `items` list.
+
+    Parameters:
+        items (list): Items to create an inventory from.
+
+    Returns:
+        dict: The inventory dictionary.
+    """
+
+    pass
+
+
+def add_items(inventory, items):
+    """Add or increment items in inventory using elements from the items `list`.
+
+    Parameters:
+        inventory (dict): Dictionary of existing inventory.
+        items (list): List of items to update the inventory with.
+
+    Returns:
+        dict: The inventory updated with the new items.
+    """
+
+    pass
+
